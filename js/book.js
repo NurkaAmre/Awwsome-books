16a71ad4a9384665d66e451bb26e414632f51a8e
NurkaAmre <107000157+NurkaAmre@users.noreply.github.com>
2022-09-28 00:07:04 +0600

    Fix linter errors

--- js/book.js
+++ js/book.js
@@ -1,105 +1,106 @@
+/* eslint-disable max-classes-per-file */
+
 // BOOK CLASS represents book
 class Book {
-    constructor(title, author) {
-        this.title = title;
-        this.author = author;
+  constructor(title, author) {
+    this.title = title;
+    this.author = author;
+  }
+}
+
+// STORE CLASS: Handles storage
+class Store {
+  static getBooks() {
+    let books;
+    if (localStorage.getItem('books') === null) {
+      books = [];
+    } else {
+      books = JSON.parse(localStorage.getItem('books'));
     }
+
+    return books;
+  }
+
+  static addBook(book) {
+    const books = Store.getBooks();
+    books.push(book);
+    localStorage.setItem('books', JSON.stringify(books));
+  }
+
+  static removeBook(index) {
+    const books = Store.getBooks();
+
+    books.forEach((book, i) => {
+      if (i === index) {
+        books.splice(index, 1);
+      }
+    });
+    localStorage.setItem('books', JSON.stringify(books));
+  }
 }
 
 // UI CLASS handle UI tasks
 class UI {
-    static displayBooks() {
-
+  static displayBooks() {
     const books = Store.getBooks();
 
-    books.forEach((book) => UI.addBookToList(book)); 
-}
+    books.forEach((book) => UI.addBookToList(book));
+  }
 
-static addBookToList(book) {
+  static addBookToList(book) {
     const list = document.querySelector('#book-list');
 
     const row = document.createElement('tr');
-     row.innerHTML = `
+    row.innerHTML = `
      <td>${book.title}</td>
      <td>${book.author}</td>
      <td><button class="delete">Delete</button></td>
      <hr>
      `;
 
-     list.appendChild(row);
+    list.appendChild(row);
   }
-  
+
   static deleteBook(el) {
-    if(el.classList.contains('delete')) {
-        el.parentElement.parentElement.remove();
+    if (el.classList.contains('delete')) {
+      el.parentElement.parentElement.remove();
+    }
   }
-}
-static clearField() {
+
+  static clearField() {
     document.querySelector('#title').value = '';
     document.querySelector('#author').value = '';
+  }
 }
-}
 
-// STORE CLASS: Handles storage
-class Store {
-    static getBooks() {
-        let books;
-        if(localStorage.getItem('books') === null){
-            books = [];
-        } else {
-            books = JSON.parse(localStorage.getItem('books'));
-        }
-
-        return books;
-    }
-
-    static addBook(book) {
-        const books = Store.getBooks();
-        books.push(book);
-        localStorage.setItem('books', JSON.stringify(books));
-    }
-
-    static removeBook(index) {
-        const books = Store.getBooks();
-
-        books.forEach((book, i) => {
-        if (i === index) {
-            books.splice(index, 1);
-        }
-    });
-    localStorage.setItem('books', JSON.stringify(books));
-}
-}
 // Event: Display Books
 document.addEventListener('DOMContentLoaded', UI.displayBooks);
 
 // Event: Add a book
 document.querySelector('#book-form').addEventListener('submit', (e) => {
-    e.preventDefault();
-    // Get form values
-    const title = document.querySelector('#title').value;
-    const author = document.querySelector('#author').value;
+  e.preventDefault();
+  // Get form values
+  const title = document.querySelector('#title').value;
+  const author = document.querySelector('#author').value;
 
-    // Instatiate book
-    const book = new Book(title, author);
+  // Instatiate book
+  const book = new Book(title, author);
 
-    // Add Book to UI
-    UI.addBookToList(book);
+  // Add Book to UI
+  UI.addBookToList(book);
 
-    // Add book to store
-    Store.addBook(book);
+  // Add book to store
+  Store.addBook(book);
 
-    // Clear fields
-    UI.clearField();
+  // Clear fields
+  UI.clearField();
 });
 
 // Event: Remove a Book
 document.querySelector('#book-list').addEventListener('click', (e) => {
-    if(e.target.classList.contains('delete')){
-        console.log("I want sex")
-        const index = [...document.querySelectorAll('.delete')].indexOf(e.target);
-        console.log(index)
-        Store.removeBook(index);
-        UI.deleteBook(e.target);
-    }
+  if (e.target.classList.contains('delete')) {
+    const index = [...document.querySelectorAll('.delete')].indexOf(e.target);
+    Store.removeBook(index);
+    UI.deleteBook(e.target);
+  }
 });
